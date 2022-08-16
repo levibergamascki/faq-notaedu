3f9f7542c5967e0ea195c1e43dba73d03a359a9f
Levi Bergamascki <82347503+levibergamascki@users.noreply.github.com>
2022-08-15 22:41:17 -0300

    teste

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Head from 'next/head';
-import Card from '../components/Card';
+import Card from './components/Card';
 
 
 function HomePage(){
